2e8eaca9d356f8a8588182e7b76e96afa8dc5307
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-03-01 15:46:24 +0900

    API取得機能の実装

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,34 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <div v-for="rental_space in rental_spaces" :key="rental_space.id">
+      レンタルスペース名：{{rental_space.name}}
+      郵便番号：{{rental_space.postal_code}}
+      住所：{{rental_space.address}}
+      建物名：{{rental_space.building}}
+      詳細：{{rental_space.detail}}
+      収容人数：{{rental_space.capacity}}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      rental_spaces: "rental_spaces"
+    }
+  },
+  mounted(){
+    this.setRentalSpace();
+  },
+  methods:{
+    setRentalSpace: function(){
+      axios.get('api/rental_spaces')
+      .then(response => (
+        this.rental_spaces = response.data
+      ))
     }
   }
 }
